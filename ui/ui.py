d12a93bb36d9db85e014afbe4557ccd3e2bc6dd1
Hannes Jensen <hannes@hannesjensen.com>
2022-11-27 20:51:14 +0100

    Remove header

--- ui/ui.py
+++ ui/ui.py
@@ -8,9 +8,6 @@ from view_model import view_model
 def _define_layout() -> ptg.Layout:
     layout = ptg.Layout()
 
-    layout.add_slot("Header", height=1)
-    layout.add_break()
-
     layout.add_slot("Body")
 
     return layout
@@ -19,10 +16,6 @@ def _define_layout() -> ptg.Layout:
 def start_ui(vm: view_model.ViewModel):
     with ptg.WindowManager() as manager:
         manager.layout = _define_layout()
-
-        manager.add(ptg.Window(
-            "[blue]My test TUI", box="EMPTY"
-        ), animate=False)
 
         todo_item_tree = widgets.TodoItemTree(vm)
 
