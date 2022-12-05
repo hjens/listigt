b152b3f684d2f96a22392c0957addb98bd8e0980
Hannes Jensen <hannes@hannesjensen.com>
2022-12-05 11:13:56 +0100

    Add footer

--- ui/ui.py
+++ ui/ui.py
@@ -8,7 +8,9 @@ from view_model import view_model
 def _define_layout() -> ptg.Layout:
     layout = ptg.Layout()
 
-    layout.add_slot("Body", height=1.0)
+    layout.add_slot("Body")
+    layout.add_break()
+    layout.add_slot("Footer", height=3)
 
     return layout
 
@@ -16,11 +18,11 @@ def _define_layout() -> ptg.Layout:
 def start_ui(vm: view_model.ViewModel):
     with ptg.WindowManager() as manager:
         manager.layout = _define_layout()
-        vm.set_window_height(manager.terminal.height - 5)
+        vm.set_window_height(manager.terminal.height - 8)
 
         todo_item_tree = widgets.TodoItemTree(vm)
 
-        body_window =ptg.Window(
+        body_window = ptg.Window(
             todo_item_tree,
             vertical_align=VerticalAlignment.TOP,
             assign="body",
@@ -28,9 +30,14 @@ def start_ui(vm: view_model.ViewModel):
 
         manager.add(body_window, animate=False)
 
+        footer_window = ptg.Window("Footer", assign="footer")
+
+        manager.add(footer_window, animate=False)
+
         def handle_key(key):
             if todo_item_tree.handle_key(key):
                 return True
             if key == "q":
                 manager.stop()
+
         manager.handle_key = handle_key
